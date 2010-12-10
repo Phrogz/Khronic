26889b42f82af241b0d8bb2c6e436be95549848e
Gavin Kistner <!@phrogz.net>
2010-12-10 09:47:37 -0700

    Updated test generator

--- test/test_wav.rb
+++ test/test_wav.rb
@@ -5,13 +5,13 @@ period = 11.0
 p2 = 10.0
 
 samples1 = (0..(2*44100)).map{ |i|
-	(amp*Math.sin(i/(period))).round
+	(amp*Math.sin(i/period)).round
 }
 samples2 = (0..(2*44100)).map{ |i|
-	(amp*Math .sin(i/(p2-=0.0001))).round
+	(amp*Math.sin(i/(p2-=0.0001))).round
 }
 
 
-w = WAV.from_samples( [samples1,samples2], :rate=>4000 )
-puts w.debug
+w = WAV.from_samples( [samples1,samples2], :rate=>22000 )
+p w
 w.write( 'sin.wav' )
