f725246aeff712e8c835725329e7ad1a1e405873
Gavin Kistner <gavin@phrogz.net>
2010-12-13 08:09:00 -0700

    Find wavs relative to document and specified path(s)

--- lib/khronic/validation.rb
+++ lib/khronic/validation.rb
@@ -1,7 +1,28 @@
 class Khronic
+	def files_for_samples
+		files = {}
+		wav_paths = [@root,''].compact
+		meta['wav_paths'].each do |path|
+			wav_paths.unshift path
+			wav_paths.unshift File.join(@root,path) if @root
+		end if meta['wav_paths']
+		samples.each do |name,file|
+			wav_paths.each do |path|
+				path = File.join(path,file)
+				if File.exists?(path)
+					files[name] = File.expand_path(path)
+					break
+				end
+			end
+			# warn "Could not find file '#{file}' for sample '#{name}'." unless files[name]
+		end
+		files
+	end
+
 	def valid_level?( n )
 		send "valid_level_#{n}?"
 	end
+	
 	def valid_level_1?
 		# Need at least one track
 		return false unless tracks.length>0
@@ -11,6 +32,13 @@ class Khronic
 		data = tracks.map{ |name,track| track.data.length }.uniq    
 		unless data.length==1
 			warn "Disparate numbers of data points: #{data.inspect}" if $DEBUG
+			return false
+		end
+		
+		files = files_for_samples
+		missing_files = samples.select{ |name,file| !files.has_key? name }
+		unless missing_files.empty?
+			warn "Could not find the following samples/files:#{missing_files}"
 			return false
 		end
 		
