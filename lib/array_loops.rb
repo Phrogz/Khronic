479da4fa7b9f6bd70c089266c40401ac16540003
Gavin Kistner <gavin@phrogz.net>
2010-12-13 07:46:27 -0700

    Changing ArrayLoops interface

--- lib/array_loops.rb
+++ lib/array_loops.rb
@@ -1,9 +1,7 @@
 module ArrayLoops
-	DEFAULTS = {from:0}
 	def loop( opts={} )
-		opts  = DEFAULTS.merge(opts)
-		start = opts[:from]
-		close = start + opts[:for]
+		start = opts[:from] || 0
+		close = start + (opts[:for] || length)
 		result = []
 		while close >= length
 			result.concat self[start..-1]
