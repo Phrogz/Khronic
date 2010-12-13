9d9dbf4ff3c3be577ab85be7c99eaa57f35d90b4
Gavin Kistner <!@phrogz.net>
2010-12-13 09:54:55 -0700

    Add simple doc to test suite constants

--- test/helper.rb
+++ test/helper.rb
@@ -2,4 +2,5 @@ require 'minitest/autorun'
 MT = MiniTest::Unit::TestCase
 docs  = File.join(File.dirname(__FILE__),'docs')
 L1DOC = File.join(docs,'cmajor_l1.yaml')
+SIMPLE = File.join(docs,'simple.yaml')
 require_relative '../lib/khronic'
